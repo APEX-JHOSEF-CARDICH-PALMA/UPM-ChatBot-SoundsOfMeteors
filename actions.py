8a63f51d1df0b5f4a25dd84124c243a0e9802748
Jhosef Cardich Palma <jacp.tech.91@gmail.com>
2020-06-16 20:27:23 +0200

    Documentacion del codigo para la definicion de los elementos necesarios para la funcionalidad de la accion de clasificacion

--- actions.py
+++ actions.py
@@ -24,7 +24,7 @@ from pygame import mixer  # Load the popular external library
 # 1  - A little explanation about how the classification works-
 #   - Explain the two options: trainnig and classification part itself
 
-# 3 - Training :
+# 2 - Training :
 #   - Explain the five types of metheor's sounds
 
 # 4 - Classification
@@ -37,14 +37,43 @@ from pygame import mixer  # Load the popular external library
 # Level 4 :
 
 
-####################################################################################################################
-# 1: Acciones que seleccionan sonidos que ya esta identificados
-####################################################################################################################
+########################################################################################################################
+## 1 - BIENVENIDA
+##----------------------------------------------------------------------------------------------------------------------
+
+##......................................................
+## Recoge el nombre del usuario
+##......................................................
+
+class ActionHelloWorld(Action):
+    """
+    ActionHelloWorld Recoge el nombre de usuario y ademas da un mensaje de bienvenida que se envia
+    al modulo conversacional del Bot para
+    """
+    def name(self) -> Text:
+        return "action_hola_mundo"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        player_name = tracker.get_slot('name')
+        dispatcher.utter_message(text="Bienvenido {} si deseas  podemos aprender, entrenar o clasificar sonidos del cielo. ¿Qué te gustaría hacer?".format(player_name))
+
+
+        return []
+
+########################################################################################################################
+## 2 - ENTRENAMIENTO
+##----------------------------------------------------------------------------------------------------------------------
 
 class ActionUnderdenseSelector(Action):
     """
-    Esta clase sirve para reproducir el sonido de un meteoro undersense.
-    Dara una breve descripcion del sonido y lo reproducira
+    En esta clase se selecciona un sonido, y se devuelve la URI del recurso de este sonido
+    asi de esta manera el Modulo ChatBot podra enviarselo al Front. Otra de las funcionalidades
+    especiales de esta clase, es que retornara, mediante el tracker, el valor de uno de los Slots
+    llamado 'sonido_actual', de esa manera, el bot sabra sobre de que tipo de sonido se esta conversando
+    con el usuario. En este caso , se devuelve la informacion del sonido de tipo underdense
     """
     def name(self) -> Text:
         return "action_underdense_sample"
@@ -65,8 +94,11 @@ class ActionUnderdenseSelector(Action):
 
 class ActionMSelector(Action):
     """
-    Esta clase sirve para reproducir el sonido de un meteoro M.
-    Dara una breve descripcion del sonido y lo reproducira
+    En esta clase se selecciona un sonido, y se devuelve la URI del recurso de este sonido
+    asi de esta manera el Modulo ChatBot podra enviarselo al Front. Otra de las funcionalidades
+    especiales de esta clase, es que retornara, mediante el tracker, el valor de uno de los Slots
+    llamado 'sonido_actual', de esa manera, el bot sabra sobre de que tipo de sonido se esta conversando
+    con el usuario. En este caso , se devuelve la informacion del sonido de tipo M
     """
     def name(self) -> Text:
         return "action_m_sample"
@@ -88,7 +120,13 @@ class ActionMSelector(Action):
 #--------------------------------------------------------------------------------------------------
 
 class ActionLongOverdenseSelector(Action):
-
+    """
+    En esta clase se selecciona un sonido, y se devuelve la URI del recurso de este sonido
+    asi de esta manera el Modulo ChatBot podra enviarselo al Front. Otra de las funcionalidades
+    especiales de esta clase, es que retornara, mediante el tracker, el valor de uno de los Slots
+    llamado 'sonido_actual', de esa manera, el bot sabra sobre de que tipo de sonido se esta conversando
+    con el usuario. En este caso , se devuelve la informacion del sonido de tipo  long over sample
+    """
     def name(self) -> Text:
         return "action_long_over_sample"
 
@@ -108,6 +146,13 @@ class ActionLongOverdenseSelector(Action):
 
 
 class ActionMediumOverdenseSelector(Action):
+    """
+    En esta clase se selecciona un sonido, y se devuelve la URI del recurso de este sonido
+    asi de esta manera el Modulo ChatBot podra enviarselo al Front. Otra de las funcionalidades
+    especiales de esta clase, es que retornara, mediante el tracker, el valor de uno de los Slots
+    llamado 'sonido_actual', de esa manera, el bot sabra sobre de que tipo de sonido se esta conversando
+    con el usuario. En este caso , se devuelve la informacion del sonido de tipo overdense medio
+    """
 
     def name(self) -> Text:
         return "action_med_over_sample"
@@ -125,6 +170,13 @@ class ActionMediumOverdenseSelector(Action):
 #--------------------------------------------------------------------------------------------------
 
 class ActionShortOverdenseSelector(Action):
+    """
+    En esta clase se selecciona un sonido, y se devuelve la URI del recurso de este sonido
+    asi de esta manera el Modulo ChatBot podra enviarselo al Front. Otra de las funcionalidades
+    especiales de esta clase, es que retornara, mediante el tracker, el valor de uno de los Slots
+    llamado 'sonido_actual', de esa manera, el bot sabra sobre de que tipo de sonido se esta conversando
+    con el usuario. En este caso , se devuelve la informacion del sonido de tipo overdense corto
+    """
 
     def name(self) -> Text:
         return "action_short_over_sample"
@@ -139,8 +191,39 @@ class ActionShortOverdenseSelector(Action):
 
         return [SlotSet("sonido_actual","5")]
 #--------------------------------------------------------------------------------------------------
-##################################################################################
 
+
+########################################################################################################################
+## 3 - BIENVENIDA
+##----------------------------------------------------------------------------------------------------------------------
+
+##......................................................
+## Escogeun sonido, teniendo en cuenta ciertos parametros
+##......................................................
+
+class ActionClassifying(Action):
+    """
+    Para el funcioamiento de esta clase, se hace un llamado a la clase soundListing
+    la cual devolvera todos los sonidos y sus datos de clasificacion. 
+    Cuando ya tenga los sonidos, esta clase analizara, quien tiene menos clasificaciones 
+    asi podra escoger los sonidos mas nuevos para enviarselos al usuario mediante 
+    el módulo conversacional
+    """
+    def name(self) -> Text:
+        return "action_classifying"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Ya estamos clasificando")
+        print("FUCK THIS ")
+        return []
+
+
+
+##......................................................
+## Recoge el nombre del usuario
+##......................................................
 
 
 
@@ -150,9 +233,13 @@ class SoundListing(object):
     Esta clase sirve para poder listar los sonidos de un directorio
     Devuelve una lista con todos los sonidos (y sus localizaciones)
     y crea un archivo de texto en el path especificado, con la misma informacion
+
+    Tambien devuelva una lista de los archivos de clasificacion, asociados a cada sonido.
+    Si un archivo de clasificacion, para un sonido dado, no existe, entonces lo crea
     """
-    soundsList = []
-    soundListFilePath = ''
+    soundsList = [] #Lista de sonidos a listar, dento de un directorio
+    soundListFilePath = '' #Archivo txt, con los nombres, linea a linea, de todos los sonidos de un directorio
+    soundClasificactionList = []
     #Constructor de la lista de sonidos
     def __init__(self,path_origen,path_destino):
         list = []
@@ -179,100 +266,9 @@ class SoundListing(object):
         return soundListSession
 
 #--------------------------------------------------------------------------------------------------
-class ActionLearning(Action):
-    """
-    Esta clase sirve para crear la Accion que sera iniciada por el Bot.
-    Reproduce los sonidos del cielo y su significado para que el usuario pueda aprender
-    """
-    def name(self) -> Text:
-        return "action_learning"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text=" Aqui escucharas algunos del cielo, asi podras aprender")
-
-        #Creamos un listado de sonidos para poder reproducirlos en cadena
-        path_origen='sounds/sonidos_entrenamiento/'
-        path_destino='sounds/sonidos_entrenamiento/'
-        listaSonidos= SoundListing.make_SoundListing(path_origen,path_destino)
-
-        mixer.init()
-        """ Hay que hacer una pequeña descripcion de los sonidos antes 
-        de poder reproducirlos, tambien hay que poner un retard de 2 
-        segundos para poder dar tiempo a escucharlos bien """
-
-
-        for sound in listaSonidos.soundsList:
-            mixer.music.load('sounds/sonidos_asistente/Bleep.wav') #El pitido de cuando empieza a ahblar el bot
-            mixer.music.play()
-
-            # Se reproduce la descripcion
-            dispatcher.utter_message(text="Este sonido es:" + sound)
-
-            # Se reproduce el sonido
-            mixer.music.load(path_origen+sound)
-            mixer.music.play()
-            time.sleep(1)
-
-        return []
-#--------------------------------------------------------------------------------------------------
 
 
 
-
-
-class ActionTraining(Action):
-
-    def name(self) -> Text:
-        return "action_training"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text=" Hola, ya estamos entrenando  !")
-        mixer.init()
-       # mixer.music.load('Sounds/dontremove.mp3')
-        mixer.music.load('sounds/sonidos_entrenamiento/meteor5_short overdense.wav')
-        mixer.music.play()
-
-        ## En este caso deberiamos saber como dirigir el flujo segun nuestro codigo aqui
-        ## y hacer preguntas al usuario par apoder ir
-        return []
-
-#--------------------------------------------------------------------------------------------------
-
-class ActionClassifying(Action):
-
-    def name(self) -> Text:
-        return "action_classifying"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Ya estamos clasificando")
-        print("FUCK THIS ")
-        return []
-
-
-####################################################################################################################
-## Recoge el nombre del usuario
-####################################################################################################################
-
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_hola_mundo"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        player_name = tracker.get_slot('name')
-        dispatcher.utter_message(text="Bienvenido {} si deseas  podemos aprender, entrenar o clasificar sonidos del cielo. ¿Qué te gustaría hacer?".format(player_name))
-
-
-        return []
 
 ####################################################################################################################
 ## Comprueba la respuesta con el sonido actual
@@ -321,3 +317,74 @@ class ActionSoundCheck(Action):
         }
         return sw.get(case, 'Opcion incorrecta')
 
+
+
+
+
+
+
+
+
+
+########################################################################################################################
+## CLASES AUXILIARES
+##----------------------------------------------------------------------------------------------------------------------
+class ActionLearning(Action):
+    """
+    EN DESHUSO, EN CASO DE QUE EL BACK REPRODUZCA EL SONIDO
+    Esta clase sirve para crear la Accion que sera iniciada por el Bot.
+    Reproduce los sonidos del cielo y su significado para que el usuario pueda aprender
+    """
+    def name(self) -> Text:
+        return "action_learning"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=" Aqui escucharas algunos del cielo, asi podras aprender")
+
+        #Creamos un listado de sonidos para poder reproducirlos en cadena
+        path_origen='sounds/sonidos_entrenamiento/'
+        path_destino='sounds/sonidos_entrenamiento/'
+        listaSonidos= SoundListing.make_SoundListing(path_origen,path_destino)
+
+        mixer.init()
+        """ Hay que hacer una pequeña descripcion de los sonidos antes 
+        de poder reproducirlos, tambien hay que poner un retard de 2 
+        segundos para poder dar tiempo a escucharlos bien """
+
+
+        for sound in listaSonidos.soundsList:
+            mixer.music.load('sounds/sonidos_asistente/Bleep.wav') #El pitido de cuando empieza a ahblar el bot
+            mixer.music.play()
+
+            # Se reproduce la descripcion
+            dispatcher.utter_message(text="Este sonido es:" + sound)
+
+            # Se reproduce el sonido
+            mixer.music.load(path_origen+sound)
+            mixer.music.play()
+            time.sleep(1)
+
+        return []
+
+
+class ActionTraining(Action):
+
+    def name(self) -> Text:
+        return "action_training"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=" Hola, ya estamos entrenando  !")
+        mixer.init()
+       # mixer.music.load('Sounds/dontremove.mp3')
+        mixer.music.load('sounds/sonidos_entrenamiento/meteor5_short overdense.wav')
+        mixer.music.play()
+
+        ## En este caso deberiamos saber como dirigir el flujo segun nuestro codigo aqui
+        ## y hacer preguntas al usuario par apoder ir
+        return []
+
+#--------------------------------------------------------------------------------------------------
